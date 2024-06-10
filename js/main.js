0fe5029e047f33938415be03a92a42e149c1a37f
Anton Pivnenko <pivnenkowebdev@gmail.com>
2024-06-10 16:46:13 +0400

    feat: add function btnControllStyle

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 const slider = document.querySelector('#slider');
 const slides = slider.querySelectorAll('[data-slide]');
 const track = slider.querySelector('#track');
+const controllList = Array.from(slider.querySelectorAll('[data-button-controll]'));
 const gapBetweenSlides = parseFloat(getComputedStyle(track).columnGap);
 const slideWidth = slides[0].offsetWidth;
 const moveWidth = slideWidth + gapBetweenSlides;
@@ -9,14 +10,26 @@ let counterSlide = 0;
 const motion = (e) => {
     const isRightArrow = e.target.closest('[data-arrow="right"]');
     const isLeftArrow = e.target.closest('[data-arrow="left"]');
+    const isControllButton = e.target.closest('[data-button-controll]');
+
+    if (isControllButton) {
+        const indexCurrentBtnControll = controllList.indexOf(e.target.closest('[data-button-controll]'));
+        btnControllStyle(isControllButton);
+        counterSlide = indexCurrentBtnControll;
+    }
 
     if (isRightArrow) {
         counterSlide = (counterSlide < slides.length - 1) ? counterSlide + 1 : 0;
     } else if (isLeftArrow) {
         counterSlide = (counterSlide > 0) ? counterSlide - 1 : slides.length - 1;
     }
-
     track.style.transform = `translateX(-${moveWidth * counterSlide}px)`;
+}
+
+const btnControllStyle = (currentBtn) => {
+    const activeBtn = slider.querySelector('button.active');
+    activeBtn.classList.remove('active');
+    currentBtn.classList.add('active');
 }
 
 slider.addEventListener('click', motion);
